013cf16600a6f05ab3c14fcf6e01c92d38c4d5f9
guillaume.favre <guillaumefavre56@gmail.com>
2021-04-10 16:58:27 +0200

    useState example

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+const InputElement = () => {
+
+    const [inputText, setInputText] = useState("")
+    const [historyInputs, setHistoryInputs] = useState([])
+
+    return  <div>
+                <input placeholder="Enter some text"
+                        onChange={(e) => {
+                            setInputText(e.target.value)
+                            setHistoryInputs([...historyInputs, e.target.value])
+                        }} />
+                <br />
+                {inputText}
+                <br />
+                <ul>
+                    {historyInputs.map((record) => {
+                        return <li>{record}</li>
+                    })}
+                </ul>
+            </div>
+}
+
+export default InputElement
